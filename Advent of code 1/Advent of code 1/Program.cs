0879a611c04f90753eb78595daae9425e010a5d4
Sam Crerar <44707197+Rocher1512@users.noreply.github.com>
2023-12-02 17:46:55 +0000

    start

---
+++ Advent of code 1/Advent of code 1/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+List<string> numbers = new List<string> { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+using (StreamReader reader = new StreamReader("input.txt"))
+{
+    int total = 0;
+    while (!reader.EndOfStream)
+    {
+        string line = reader.ReadLine();
+        string number = "";
+        number += lfind(line.ToLower());
+        number += rfind(line.ToLower());
+        total += Int32.Parse(number);
+        Console.WriteLine($"{line} :: {number}");
+    }
+    Console.WriteLine(total);
+}
+
+
+char lfind(string line)
+{
+    int endIndex = int.MaxValue;
+    char output = 'X';
+    for (int i = 0; i < 9; i++) 
+    {
+        if (line.Contains(numbers[i]))
+        {
+            int newEndIndex = line.IndexOf(numbers[i]);
+            if (newEndIndex < endIndex) 
+            {
+                endIndex = newEndIndex;
+                output = char.Parse((i + 1).ToString());
+            }
+        }
+    }
+    for (int i = 0; i < endIndex; i++)
+    {
+        if (Char.IsDigit(line[i]))
+        {
+            return output = line[i];
+        }
+    }
+    return output;
+}
+
+char rfind(string line)
+{
+
+    int startIndex = int.MinValue;
+    char output = 'X';
+    for (int i = 0; i < 9; i++)
+    {
+        if (line.Contains(numbers[i]))
+        {
+            string inputString = line;
+            string searchString = numbers[i];
+            int newStartIndex = line.Select((c, i) => inputString.Substring(i))
+                                               .Select((substring, index) => new { substring, index })
+                                               .Where(pair => pair.substring.StartsWith(searchString))
+                                               .Select(pair => pair.index)
+                                               .DefaultIfEmpty(-1)
+                                               .Max();
+            if (newStartIndex > startIndex)
+            {
+                startIndex = newStartIndex;
+                output = char.Parse((i + 1).ToString());
+            }
+        }
+    }
+    for (int i = line.Length - 1; i >= startIndex; i--)
+    {
+        if (Char.IsDigit(line[i]))
+        {
+            return output = line[i];
+        }
+    }
+    return output;
+}
---
+++ Advent of code 2/Advent of code 2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Linq.Expressions;
+
+int redLimit = 12;
+int greenLimit = 13;
+int blueLimit = 14;
+
+int total = 0;
+int counter = 1;
+using (StreamReader reader = new StreamReader("input.txt"))
+{
+    
+    while (!reader.EndOfStream)
+    {
+        string line = reader.ReadLine();
+        line = line.Split(':')[1];
+        string[] hands = line.Split(';');
+        bool add = true;
+        for (int i = 0; i < hands.Count(); i++)
+        {
+            string[] pulls = hands[i].Split(',');
+            
+            for (int j = 0; j < pulls.Count(); j++)
+            {
+                string[] components = pulls[j].Trim().Split(' ');
+
+                switch (components[1])
+                {
+                    case "red":
+                        if (int.Parse(components[0]) > redLimit)
+                        {
+                            add = false;
+                        }
+                        break;
+                    case "green":
+                        if (int.Parse(components[0]) > greenLimit)
+                        {
+                            add = false;
+                        }
+                        break;
+                    case "blue":
+                        if (int.Parse(components[0]) > blueLimit)
+                        {
+                            add = false;
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Oh no");
+                        break;
+                }
+            }
+        }
+        if (add)
+        {
+            total += counter;
+        }
+        counter++;
+    }
+}
+Console.WriteLine(total);
